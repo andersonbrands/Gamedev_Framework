f8c0d39c9395e83dad77d72ad5903168ff9abf26
Anderson Brandao <anderson.brands@gmail.com>
2014-10-13 21:29:33 -0300

    Creating an empty project, ready to use DirectX
    Creating an empty project, ready to use DirectX

---
+++ GamedevFramework/GamedevFramework/src/Main.cpp
@@ -0,0 +1,13 @@
+/***************************************************************************************
+*	Title: Main.cpp
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int) {
+
+    return 0;
+}
