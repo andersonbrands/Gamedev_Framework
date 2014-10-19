a02ccd93b39902a4f5460d630d4d021b3628d7c3
Anderson Brandao <anderson.brands@gmail.com>
2014-10-18 21:04:25 -0300

    Adding SpriteManager
    Adding SpriteManager

---
+++ GamedevFramework/GamedevFramework/src/Framework/Renderer/Sprite/SpriteId.h
@@ -0,0 +1,20 @@
+/***************************************************************************************
+*	Title: SpriteId.h
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#ifndef SPRITE_ID_H_
+#define SPRITE_ID_H_
+
+namespace Framework {
+
+    typedef unsigned int SpriteId;
+
+    namespace spr {
+        //static const SpriteId SPRITE_NAME = id;
+        static const SpriteId SAMPLE = 0;
+    }
+}
+#endif // SPRITE_ID_H_
---
+++ GamedevFramework/GamedevFramework/src/Framework/Renderer/Sprite/SpriteManager.h
@@ -0,0 +1,35 @@
+/***************************************************************************************
+*	Title: SpriteManager.h
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#ifndef SPRITE_MANAGER_H_
+#define SPRITE_MANAGER_H_
+
+#include "../../Utilities/UnorderedMapContainer.h"
+#include "../../Utilities/Singleton.h"
+#include "SpriteId.h"
+#include "Sprite.h"
+
+namespace Framework {
+
+    class SpriteManager : UnorderedMapContainer<SpriteId, Sprite>, public Singleton<SpriteManager> {
+        private:
+        public:
+            SpriteManager();
+            virtual ~SpriteManager();
+
+            Sprite* addSprite(const SpriteId &id) {
+                return addObject(id);
+            }
+
+            Sprite* getSprite(const SpriteId &id) {
+                return getObject(id);
+            }
+    };
+
+}
+
+#endif // SPRITE_MANAGER_H_
