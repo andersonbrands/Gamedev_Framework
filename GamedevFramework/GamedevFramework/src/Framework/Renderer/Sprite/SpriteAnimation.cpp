465c611a223abd0a8179dba6ea1b1db24bc4fc8f
Anderson Brandao <anderson.brands@gmail.com>
2014-10-18 21:09:26 -0300

    Adding SpriteAnimation
    Adding SpriteAnimation

---
+++ GamedevFramework/GamedevFramework/src/Framework/Renderer/Sprite/SpriteAnimation.cpp
@@ -0,0 +1,83 @@
+/***************************************************************************************
+*	Title: SpriteAnimation.cpp
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#include "SpriteAnimation.h"
+#include "../../Utilities/Timer.h"
+#include <cassert>
+
+namespace Framework {
+
+    SpriteAnimation::SpriteAnimation(unsigned int totalFrames, float fps) : totalFrames_(totalFrames), currentFrame_(0), fps_(fps), spf_(1/fps), loop_(true), playing_(false), deltaTime_(0.0f), spriteV_() {
+        spriteV_.reserve(totalFrames_);
+    }
+
+    SpriteAnimation::~SpriteAnimation() {
+        spriteV_.clear();
+    }
+
+    void SpriteAnimation::addFrame(Sprite* pSprite) {
+        spriteV_.push_back(pSprite);
+    }
+
+    void SpriteAnimation::goToAndPlay(int frame) {
+        assert(frame <= totalFrames_);
+        currentFrame_ = frame;
+        play();
+    }
+    void SpriteAnimation::goToAndPause(int frame) {
+        assert(frame <= totalFrames_);
+        currentFrame_ = frame;
+        pause();
+    }
+
+    void SpriteAnimation::play() {
+        playing_ = true;
+        if (currentFrame_ == 0) {
+            currentFrame_ = 1;
+        }
+    }
+
+    void SpriteAnimation::stop() {
+        playing_ = false;
+        currentFrame_ = 0;
+        deltaTime_ = 0.0f;
+    }
+
+    void SpriteAnimation::pause() {
+        playing_ = false;
+    }
+
+    void SpriteAnimation::update() {
+        if (!currentFrame_ || !playing_) {
+            return;
+        }
+
+        assert(Timer::getInstancePtr());
+        float delta = Timer::getInstancePtr()->getTimeSim();
+
+        deltaTime_ += delta;
+
+        if (deltaTime_ > spf_) {
+            ++currentFrame_;
+            deltaTime_ -= spf_;
+        }
+
+        if (currentFrame_ > totalFrames_) {
+            if (loop_)
+                currentFrame_ = 1;
+            else
+                stop();
+        }
+
+    }
+
+    void SpriteAnimation::render() {
+        if (currentFrame_)
+            spriteV_.at(currentFrame_ - 1)->render();
+    }
+
+}
---
+++ GamedevFramework/GamedevFramework/src/Framework/Renderer/Sprite/SpriteAnimation.h
@@ -0,0 +1,55 @@
+/***************************************************************************************
+*	Title: SpriteAnimation.h
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#ifndef SPRITE_ANIMATION_H_
+#define SPRITE_ANIMATION_H_
+
+#include "Sprite.h"
+#include <vector>
+#include "../Texture/TextureId.h"
+#include "../../Math/Vector3.h"
+
+using namespace std;
+
+namespace Framework {
+
+    class SpriteAnimation {
+        private:
+            unsigned int totalFrames_;
+            // starts count at 1, zero means no frame is to be displayed
+            unsigned int currentFrame_;
+            float fps_;
+            float spf_;
+            bool loop_;
+            bool playing_;
+            float deltaTime_;
+
+            vector<Sprite*> spriteV_;
+        public:
+            SpriteAnimation(unsigned int totalFrames, float fps);
+            virtual ~SpriteAnimation();
+
+            void addFrame(Sprite* pSprite);
+
+            unsigned int getCurrentFrame() {
+                return currentFrame_;
+            }
+
+            void goToAndPlay(int frame);
+            void goToAndPause(int frame);
+            void play();
+            void stop();
+            void pause();
+
+            void update();
+
+            void render();
+    };
+
+}
+
+#endif // SPRITE_ANIMATION_H_
