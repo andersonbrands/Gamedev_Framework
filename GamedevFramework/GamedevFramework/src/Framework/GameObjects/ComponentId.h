cfd15f7d04c251b8ae67aa035daf2817bd508a19
Anderson Brandao <anderson.brands@gmail.com>
2014-10-19 16:49:22 -0200

    Adding GameObject and Component interfaces
    Adding GameObject and Component interfaces

---
+++ GamedevFramework/GamedevFramework/src/Framework/GameObjects/ComponentId.h
@@ -0,0 +1,14 @@
+#ifndef COMPONENT_ID_H_
+#define COMPONENT_ID_H_
+
+namespace Framework {
+    typedef unsigned int ComponentId;
+
+    namespace comp {
+        //static const ComponentId COMPONENT_ID_COMPONENT_NAME	= value;
+        static const ComponentId SAMPLE = 0;
+
+    }
+}
+
+#endif // COMPONENT_ID_H_
