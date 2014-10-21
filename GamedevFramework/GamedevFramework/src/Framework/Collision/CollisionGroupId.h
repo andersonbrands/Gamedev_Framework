1eaa3d07fce8ab146b480d2bcd49631da25b7ca9
Anderson Brandao <anderson.brands@gmail.com>
2014-10-20 23:31:40 -0200

    Adding CollisionGroupId
    Adding CollisionGroupId

---
+++ GamedevFramework/GamedevFramework/src/Framework/Collision/CollisionGroupId.h
@@ -0,0 +1,19 @@
+/***************************************************************************************
+*	Title: CollisionGroupId.h
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#ifndef COLLISION_GROUP_ID_H_
+#define COLLISION_GROUP_ID_H_
+
+namespace Framework {
+
+    typedef unsigned int CollisionGroupId;
+
+    // static const CollisionGroupId GROUP_NAME		= value;
+    static const CollisionGroupId SAMPLE			= 0;
+}
+
+#endif // COLLISION_GROUP_ID_H_
