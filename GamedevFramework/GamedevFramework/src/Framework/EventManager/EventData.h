a2152de97d683d68c2976956d2baae50458ff751
Anderson Brandao <anderson.brands@gmail.com>
2014-10-22 23:31:58 -0200

    Adding EventData
    Adding EventData

---
+++ GamedevFramework/GamedevFramework/src/Framework/EventManager/EventData.h
@@ -0,0 +1,28 @@
+/***************************************************************************************
+*	Title: EventData.h
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#ifndef EVENT_DATA_H_
+#define EVENT_DATA_H_
+
+#include "../Utilities/Utils.h"
+#include "../GameObjects/GameObject.h"
+
+namespace Framework {
+
+    namespace ev {
+        namespace data {
+
+            struct CollisionEventData {
+                GameObject* pGameObject;
+            };
+
+        }
+    }
+
+}
+
+#endif // EVENT_DATA_H_
