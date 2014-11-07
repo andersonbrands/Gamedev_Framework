f7b705029703b25e70ef0d8405d5fae182c9a4d0
Anderson Brandao <anderson.brands@gmail.com>
2014-11-07 17:45:48 -0200

    Addin the Framework namespace
    Addin the Framework namespace

--- GamedevFramework/GamedevFramework/src/Framework/Scene/iScene.h
+++ GamedevFramework/GamedevFramework/src/Framework/Scene/iScene.h
@@ -8,18 +8,22 @@
 #ifndef I_SCENE_H_
 #define I_SCENE_H_
 
-class iScene {
-    private:
-    public:
-        iScene() {}
-        virtual ~iScene() {}
+namespace Framework {
 
-        virtual void load()		= 0;
-        virtual void init()		= 0;
-        virtual void update()	= 0;
-        virtual void unload()	= 0;
-        virtual void dispose()	= 0;
+    class iScene {
+        private:
+        public:
+            iScene() {}
+            virtual ~iScene() {}
 
-};
+            virtual void load()		= 0;
+            virtual void init()		= 0;
+            virtual void update()	= 0;
+            virtual void unload()	= 0;
+            virtual void dispose()	= 0;
+
+    };
+
+}
 
 #endif // I_SCENE_H_
