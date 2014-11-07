7dfb74c1fffb3a659410de13fc2ed3749a53ab5b
Anderson Brandao <anderson.brands@gmail.com>
2014-11-07 17:44:42 -0200

    Adding a Scene interface
    Adding a Scene interface

---
+++ GamedevFramework/GamedevFramework/src/Framework/Scene/iScene.h
@@ -0,0 +1,25 @@
+/***************************************************************************************
+*	Title: iScene.h
+*	Author: Brandao, Anderson
+*	Date: 2014
+*
+***************************************************************************************/
+
+#ifndef I_SCENE_H_
+#define I_SCENE_H_
+
+class iScene {
+    private:
+    public:
+        iScene() {}
+        virtual ~iScene() {}
+
+        virtual void load()		= 0;
+        virtual void init()		= 0;
+        virtual void update()	= 0;
+        virtual void unload()	= 0;
+        virtual void dispose()	= 0;
+
+};
+
+#endif // I_SCENE_H_
