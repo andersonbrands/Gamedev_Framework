c24ec3dd0920d906c58c16b6c19acd4ae1f45084
Anderson Brandao <anderson.brands@gmail.com>
2014-11-08 18:22:23 -0200

    Adding some sprite ids
    Adding some sprite ids

--- GamedevFramework/GamedevFramework/src/Game/Ids/SpriteIds.h
+++ GamedevFramework/GamedevFramework/src/Game/Ids/SpriteIds.h
@@ -14,7 +14,9 @@ using namespace Framework;
 
 namespace spr {
     //static const SpriteId SPRITE_NAME = id;
-    static const SpriteId SAMPLE = 0;
+    static const SpriteId SAMPLE				= 0;
+    static const SpriteId MAIN_MENU_BACKGROUND	= 1;
+    static const SpriteId MAIN_MENU_GAME_NAME	= 2;
 }
 
 #endif // SPRITE_IDS_H_
